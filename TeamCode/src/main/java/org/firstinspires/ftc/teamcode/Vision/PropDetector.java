148ce32d946fb396e5e2f4c3422d8802f5bb595f
Arav-R <63694299+Arav-R@users.noreply.github.com>
2023-09-10 18:37:48 -0500

    fixed up the vision stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/PropDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/PropDetector.java
@@ -29,13 +29,11 @@
 
 package org.firstinspires.ftc.teamcode.Vision;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
-import org.firstinspires.ftc.vision.VisionProcessor;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
@@ -49,7 +47,7 @@ import java.util.List;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
 @TeleOp(name = "Concept: AprilTag", group = "Concept")
-@Disabled
+//@Disabled
 public class PropDetector extends LinearOpMode {
 
     private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
@@ -57,7 +55,7 @@ public class PropDetector extends LinearOpMode {
     /**
      * The variable to store our instance of the AprilTag processor.
      */
-    private VisionProcessor pipeline;
+//    private AprilTagProcessor aprilTag;
 
     /**
      * The variable to store our instance of the vision portal.
@@ -67,10 +65,7 @@ public class PropDetector extends LinearOpMode {
     @Override
     public void runOpMode() {
 
-        VisionPortal myVisionPortal;
-
-        VisionPortal portal = new VisionPortal.Builder().addProcessor(pipeline).build();
-
+        initProcessor();
 
         // Wait for the DS start button to be touched.
         telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
@@ -81,6 +76,11 @@ public class PropDetector extends LinearOpMode {
         if (opModeIsActive()) {
             while (opModeIsActive()) {
 
+
+
+                // Push telemetry to the Driver Station.
+                telemetry.update();
+
                 // Save CPU resources; can resume streaming when needed.
                 if (gamepad1.dpad_down) {
                     visionPortal.stopStreaming();
@@ -88,12 +88,55 @@ public class PropDetector extends LinearOpMode {
                     visionPortal.resumeStreaming();
                 }
 
+                // Share the CPU.
+                sleep(20);
             }
         }
 
+        // Save more CPU resources when camera is no longer needed.
+        visionPortal.close();
 
     }   // end method runOpMode()
 
+    /**
+     * Initialize the AprilTag processor.
+     */
+    private void initProcessor() {
+
+        // Create the vision portal by using a builder.
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+
+        // Set the camera (webcam vs. built-in RC phone camera).
+        if (USE_WEBCAM) {
+            builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        } else {
+            builder.setCamera(BuiltinCameraDirection.BACK);
+        }
+
+        // Choose a camera resolution. Not all cameras support all resolutions.
+        //builder.setCameraResolution(new Size(640, 480));
+
+        // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
+        //builder.enableCameraMonitoring(true);
+
+        // Set the stream format; MJPEG uses less bandwidth than default YUY2.
+        //builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
+
+        // Choose whether or not LiveView stops if no processors are enabled.
+        // If set "true", monitor shows solid orange screen if no processors enabled.
+        // If set "false", monitor shows camera view without annotations.
+        //builder.setAutoStopLiveView(false);
+
+        // Set and enable the processor.
+        builder.addProcessor(new SimpleThresholdProcessor(telemetry));
+
+        // Build the Vision Portal, using the above settings.
+        visionPortal = builder.build();
+
+        // Disable or re-enable the aprilTag processor at any time.
+        //visionPortal.setProcessorEnabled(aprilTag, true);
+
+    }   // end method initAprilTag()
 
 
 }   // end class
