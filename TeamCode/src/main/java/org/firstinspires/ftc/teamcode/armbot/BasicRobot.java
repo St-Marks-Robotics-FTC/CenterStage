4121ee28b5d7178411f6e90e0f4a1b2782eab58d
Arav-R <63694299+Arav-R@users.noreply.github.com>
2023-11-09 15:42:47 -0600

    teleop code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armbot/BasicRobot.java
@@ -0,0 +1,101 @@
+package org.firstinspires.ftc.teamcode.armbot;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Config
+@TeleOp
+public class BasicRobot extends LinearOpMode {
+
+    public static double clawOpen = 0;
+    public static double clawClosed = 0.5;
+
+    public static int armUp = 1000;
+    public static int armDown = 0;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+
+        DcMotor armMotor = hardwareMap.dcMotor.get("arm");
+
+        // claw servo
+        Servo clawServo = hardwareMap.servo.get("claw");
+
+        // Reverse the right side motors. This may be wrong for your setup.
+        // If your robot moves backwards when commanded to go forwards,
+        // reverse the left side instead.
+        // See the note about this earlier on this page.
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // reset arm
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x + 0.15 * gamepad1.right_trigger - 0.15 * gamepad1.left_trigger;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            if (gamepad1.left_bumper) {
+                frontLeftMotor.setPower(0.3 * frontLeftPower);
+                backLeftMotor.setPower(0.3 * backLeftPower);
+                frontRightMotor.setPower(0.3 * frontRightPower);
+                backRightMotor.setPower(0.3 * backRightPower);
+            } else {
+                frontLeftMotor.setPower(frontLeftPower);
+                backLeftMotor.setPower(backLeftPower);
+                frontRightMotor.setPower(frontRightPower);
+                backRightMotor.setPower(backRightPower);
+            }
+
+
+
+            if (gamepad1.y) {
+                armMotor.setTargetPosition(armUp);
+                armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                armMotor.setPower(0.5);
+            } else if (gamepad1.a) {
+                armMotor.setTargetPosition(armDown);
+                armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                armMotor.setPower(0.5);
+            }
+
+            if (gamepad1.x) {
+                clawServo.setPosition(clawClosed);
+            } else if (gamepad1.b) {
+                clawServo.setPosition(clawOpen);
+            }
+
+        }
+    }
+}
