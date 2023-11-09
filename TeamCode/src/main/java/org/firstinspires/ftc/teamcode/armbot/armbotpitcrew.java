3c07d6eb381207593fccaba6a952c762dcb91ccf
MichaelYang469 <ymichael469@gmail.com>
2023-11-09 15:39:31 -0600

    lm1 robot shit pt 2

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armbot/armbotpitcrew.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.armbot;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class armbotpitcrew extends OpMode {
+
+    Arm robot;
+
+    @Override
+    public void init() {
+        robot = new Arm(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.dpad_up) {
+            robot.arm.setPower(0.3);
+        }
+        if (gamepad1.dpad_down) {
+            robot.arm.setPower(-0.3);
+        }
+        telemetry.addData("arm pos: ", robot.arm.getCurrentPosition());
+    }
+}
