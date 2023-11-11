b21da146a9dbf90c65fd690404a22b94dda573b4
Arav-R <63694299+Arav-R@users.noreply.github.com>
2023-11-10 22:57:33 -0600

    testing techtoolbox

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armbot/CameraTest.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.armbot;
+
+import android.util.Size;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.Vision.RedPropThreshold;
+import org.firstinspires.ftc.vision.VisionPortal;
+
+
+@Autonomous(name="Vision Test")
+public class CameraTest extends LinearOpMode {
+
+    private VisionPortal portal;
+    private RedPropThreshold redPropThreshold;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        portal = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .setCameraResolution(new Size(640, 480))
+                .setCamera(BuiltinCameraDirection.BACK)
+                .addProcessor(redPropThreshold)
+                .build();
+
+
+        waitForStart();
+        telemetry.addData("Prop Position", redPropThreshold.getPropPosition());
+        telemetry.update();                        //Will output prop position on Driver Station Console
+
+
+
+
+    }
+}
