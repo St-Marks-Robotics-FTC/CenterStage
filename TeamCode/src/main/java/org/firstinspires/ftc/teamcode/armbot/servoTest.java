7099e103775d975d0e5ca46dc85b5755830b0573
Arav-R <63694299+Arav-R@users.noreply.github.com>
2023-12-07 12:17:59 -0600

    added MultipleTelemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armbot/servoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armbot/servoTest.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode.armbot;
 
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -24,6 +26,7 @@ public class servoTest extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
         waitForStart();
 
