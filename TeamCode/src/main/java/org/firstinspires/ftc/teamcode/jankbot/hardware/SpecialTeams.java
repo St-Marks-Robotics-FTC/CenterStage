32741a12ad29b9b51dd3ea460d7d73e3640a2020
Arav-R <63694299+Arav-R@users.noreply.github.com>
2023-12-20 00:57:43 -0600

    cleaned SpecialTeams

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jankbot/hardware/SpecialTeams.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jankbot/hardware/SpecialTeams.java
@@ -1,36 +1,55 @@
 package org.firstinspires.ftc.teamcode.jankbot.hardware;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+@Config
 public class SpecialTeams {
 
     DcMotorEx hang;
     Servo drone;
 
-    private double hangPower=1.0;
-    private double loadedPos=0.3;
-    private double releasePos=0.5;
+    // Hang
+    public static int hangPrepare = 700;
+    public static int hangHold = 1200;
+    public static double hangPower = 1.0;
+
+    // Drone
+    public static double holdPos = 0.0;
+    public static double releasePos = 0.5;
 
     public SpecialTeams (HardwareMap hardwareMap) {
         hang = hardwareMap.get(DcMotorEx.class, "hang");
         drone = hardwareMap.get(Servo.class, "drone");
 
         hang.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        hang.setTargetPositionTolerance(10);
-        drone.setPosition(loadedPos);
+        hang.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
-    public void setHang(int pos) {
-        hang.setTargetPosition(pos);
+    // Hang
+    public void hangReady() {
+        hang.setTargetPosition(hangPrepare);
+        hang.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        hang.setPower(hangPower);
+    }
+
+    public void hangHold() {
+        hang.setTargetPosition(hangHold);
         hang.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         hang.setPower(hangPower);
     }
 
     public int getHangPos() {
         return hang.getCurrentPosition();
+    }
+
+
+    // Drone
+    public void holdDrone() {
+        drone.setPosition(holdPos);
     }
 
     public void releaseDrone() {
