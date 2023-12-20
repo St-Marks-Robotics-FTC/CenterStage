9c96d11e6ff5ae6104884ed29ce96b62938d5d3a
Arav-R <63694299+Arav-R@users.noreply.github.com>
2023-12-20 00:49:51 -0600

    cleaned up Intake class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jankbot/hardware/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jankbot/hardware/Intake.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.jankbot.hardware;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -7,86 +8,83 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 //import org.firstinspires.ftc.teamcode.jankbot.util.ActionQueue;
 
+@Config
 public class Intake {
 
     public DcMotorEx intake;
     public Servo dropdown;
-    public Servo transfer;
-    public Servo transferClaw;
+    public Servo tilt;
+    public Servo lock;
 
-    private double clawLeftClosed = 0.65;
-    private double clawLeftOpen = 0.8;
-    private double clawRightClosed = 0.34;
-    private double clawRightOpen = 0.25;
 
-    private double transferDown=0.2;
-    private double transferPos = 0.3;
-    private double transferClose = 0.4;
-    private double transferRelease = 0.5;
+    public static double dropDown = 0.5;
+    public static double dropUp = 0.0;
 
-    public enum intakeState {
-        INTAKE,
-        IDLE,
-        TRANSFER
-    }
+    public static double transferLock = 0.5;
+    public static double transferUnlock = 0.3;
 
-    public intakeState status;
-    private double targetPower;
+    public static double transferUp = 0.4;
+    public static double transferDown=0.2;
 
-//    private ActionQueue queue;
-
-    private int transferDelay = 1000;
 
     public Intake(HardwareMap hardwareMap) {
         //intake
         intake = hardwareMap.get(DcMotorEx.class, "intake");
         dropdown = hardwareMap.get(Servo.class, "dropdown");
         //transfer
-        transfer = hardwareMap.get(Servo.class, "transfer");
-        transferClaw = hardwareMap.get(Servo.class, "transferClaw");
+        tilt = hardwareMap.get(Servo.class, "tilt");
+        lock = hardwareMap.get(Servo.class, "lock");
 
-//        queue = new ActionQueue();
 
-        intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        transfer.setPosition(transferDown);
+        intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        status = intakeState.IDLE;
-        targetPower=0.0;
     }
 
-    public void update() {
-        if (status!=intakeState.TRANSFER) {
-            if (targetPower>0) {
-                status=intakeState.INTAKE;
-            } else {
-                status=intakeState.IDLE;
-            }
-        }
 
-//        queue.update();
-    }
-
-    public void resetQueue() {
-//        queue.clearQueue();
-    }
-
+    // Intake Motor
     public void setIntake(double power) {
         intake.setPower(power);
-        status = intakeState.INTAKE;
-        targetPower=power;
     }
 
     public double getPower() {
-        return targetPower;
+        return intake.getPower();
     }
 
-    public void transfer() {
-        transfer.setPosition(transferPos);
-//        queue.addDelayedAction(() -> transferRelease(), transferDelay);
-        status = intakeState.TRANSFER;
+    public double getVelocity() {
+        return intake.getVelocity();
     }
 
-    public void transferRelease() {
-        transferClaw.setPosition(transferRelease);
+    // Intake Dropdown
+    public void dropdown() {
+        dropdown.setPosition(dropDown);
     }
+    public void raise() {
+        dropdown.setPosition(dropUp);
+    }
+
+
+    // Transfer Lock
+    public void lock() {
+        lock.setPosition(transferLock);
+    }
+
+    public void unlock() {
+        lock.setPosition(transferUnlock);
+    }
+
+
+
+
+
+
+    // Transfer Tilt
+    public void tiltUp() {
+        tilt.setPosition(transferUp);
+    }
+
+    public void tiltDown() {
+        tilt.setPosition(transferDown);
+    }
+
+
 }
