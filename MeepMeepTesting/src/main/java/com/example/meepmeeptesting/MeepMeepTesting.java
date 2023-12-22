a8ed150ae720944f6b295b79e251687fa009f463
Arav-R <63694299+Arav-R@users.noreply.github.com>
2023-12-22 02:45:26 -0600

    right far 2+5 auto

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -14,9 +14,57 @@ public class MeepMeepTesting {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(50, 50, Math.toRadians(120), Math.toRadians(120), 15)
+                .setDimensions(18, 18) // Set size of bot
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-38, 60, Math.toRadians(-90))) // Red
+                        drive.trajectorySequenceBuilder(new Pose2d(-38, -60, Math.toRadians(-90))) // Red
+
+                                .setReversed(true)
+                                .splineToSplineHeading(new Pose2d(-31, -34, Math.toRadians(180)), Math.toRadians(35))
+                                .waitSeconds(0.5) // place purple
+
+
+                                .setReversed(false)
+                                .setTangent(Math.toRadians(170))
+                                .splineToConstantHeading(new Vector2d(-60, -11.5), Math.toRadians(180)) // go to stack
+                                .waitSeconds(0.5) // intake stack
+
+                                .setReversed(true)
+                                .splineToSplineHeading(new Pose2d(24, -11.5, Math.toRadians(180)), Math.toRadians(0))
+//                                .splineToConstantHeading(new Vector2d(45, -43), Math.toRadians(0))
+                                .splineTo(new Vector2d(45, -43), Math.toRadians(0))
+                                .waitSeconds(1.5) // place pixels
+
+
+                                .setReversed(false)
+//                                .splineToSplineHeading(new Pose2d(24, -11.5, Math.toRadians(180)), Math.toRadians(180))
+                                .splineTo(new Vector2d(24, -11.5), Math.toRadians(180))
+                                .splineToConstantHeading(new Vector2d(-60, -11.5), Math.toRadians(180)) // go to stack
+                                .waitSeconds(0.5) // intake stack
+
+                                .setReversed(true)
+                                .splineToSplineHeading(new Pose2d(15, -11.5, Math.toRadians(180)), Math.toRadians(0))
+                                .splineToSplineHeading(new Pose2d(38, -11.5, Math.toRadians(150)), Math.toRadians(0))
+                                .waitSeconds(0.5) // place pixels
+
+
+
+                                .setReversed(false)
+                                .setTangent(Math.toRadians(180))
+//                                .splineToSplineHeading(new Pose2d(24, -11.5, Math.toRadians(180)), Math.toRadians(180))
+                                .splineToSplineHeading(new Pose2d(10, -11.5, Math.toRadians(180)), Math.toRadians(180)) // go to stack
+                                .splineToSplineHeading(new Pose2d(-60, -11.5, Math.toRadians(180)), Math.toRadians(180)) // go to stack
+                                .waitSeconds(0.5) // intake stack
+
+                                .setReversed(true)
+                                .splineToSplineHeading(new Pose2d(15, -11.5, Math.toRadians(180)), Math.toRadians(0))
+                                .splineToSplineHeading(new Pose2d(38, -11.5, Math.toRadians(150)), Math.toRadians(0))
+                                .waitSeconds(0.5) // place pixels
+
+
+
+
+
 
 
 
