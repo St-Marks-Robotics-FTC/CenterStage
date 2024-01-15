168d7315ebba28f18e9d8f79358e10a7d5f8181d
MichaelYang469 <ymichael469@gmail.com>
2024-01-15 17:16:03 -0600

    minor changes

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -14,52 +14,154 @@ public class MeepMeepTesting {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(50, 40, Math.toRadians(120), Math.toRadians(120), 15)
+                .setConstraints(65, 56, Math.toRadians(120), Math.toRadians(120), 15)
                 .setDimensions(13.5, 14) // Set size of bot
                 .followTrajectorySequence(drive ->
-//                        drive.trajectorySequenceBuilder(new Pose2d(16.5, 63.5, Math.toRadians(90))) // Blue Close Starting Position
-                        drive.trajectorySequenceBuilder(new Pose2d(-40, -63.5, Math.toRadians(-90))) // Red Close Starting Position
+                        drive.trajectorySequenceBuilder(new Pose2d(-40, 63, Math.toRadians(90))) // Blue Far
 
 
 
+//                                .splineToSplineHeading(new Pose2d(-34, -32, Math.toRadians(180)), Math.toRadians(35))
+//                                // place purple from back
 
 
-//                                // Blue close 2+0
+                        //drive.trajectorySequenceBuilder(new Pose2d(17, 63.5, Math.toRadians(-90))) // Blue Close
+                                //close
 //                                .setReversed(true)
-//                                .splineToSplineHeading(new Pose2d(28, 36, Math.toRadians(-135)), Math.toRadians(-45))
-//                                .waitSeconds(1) // Purple Spike
+//                                .setTangent(Math.toRadians(-90))
+//                                .splineToSplineHeading(new Pose2d(23, 42, Math.toRadians(90)), Math.toRadians(-90))
+//                                .setTangent(Math.toRadians(15))
+//                                //.splineToConstantHeading(new Vector2d(11, 32), Math.toRadians(-80))
+//                                .splineToSplineHeading(new Pose2d(48, 30, Math.toRadians(180)), Math.toRadians(-30))
+                                //far
+//                                .setTangent(Math.toRadians(-110))
+//                                .splineToSplineHeading(new Pose2d(-42, 40, Math.toRadians(60)), Math.toRadians(-110))
+//                                .setTangent(Math.toRadians(45))
+//                                .splineToLinearHeading(new Pose2d(-26, 12, Math.toRadians(180)), Math.toRadians(0))
+//                                .setTangent(0)
+//                                .splineToConstantHeading(new Vector2d(25, 12) , Math.toRadians(0))
+//                                .splineToConstantHeading(new Vector2d(48, 30), Math.toRadians(0))
+                                //close side cycle
+//                                .setReversed(true)
+//                                .setTangent(Math.toRadians(-90))
+//                                .splineToSplineHeading(new Pose2d(23, 42, Math.toRadians(90)), Math.toRadians(-90))
+//                                .waitSeconds(1)
+//                                .setTangent(Math.toRadians(15))
+//                                //.splineToConstantHeading(new Vector2d(11, 32), Math.toRadians(-80))
+//                                .splineToSplineHeading(new Pose2d(48, 30, Math.toRadians(180)), Math.toRadians(-30))
+//                                .waitSeconds(1)
+//                                .setTangent(Math.toRadians(-135))
+//                                .splineToConstantHeading(new Vector2d(20, 12), Math.toRadians(180))
+//                                .splineToConstantHeading(new Vector2d(-61, 12), Math.toRadians(180))
+//                                .waitSeconds(2)
+//                                .setTangent(Math.toRadians(0))
+//                                .splineToConstantHeading(new Vector2d(20, 12), Math.toRadians(0))
+//                                .splineToConstantHeading(new Vector2d(48, 34), Math.toRadians(45))
+//                                .waitSeconds(1)
+//                                .setTangent(Math.toRadians(-135))
+//                                .splineToConstantHeading(new Vector2d(20, 12), Math.toRadians(180))
+//                                .splineToConstantHeading(new Vector2d(-61, 12), Math.toRadians(180))
+//                                .waitSeconds(2)
+//                                .setTangent(Math.toRadians(0))
+//                                .splineToConstantHeading(new Vector2d(20, 12), Math.toRadians(0))
+//                                .splineToConstantHeading(new Vector2d(48, 34), Math.toRadians(45))
+//                                .waitSeconds(1)
+
+                                //far cycle
+                                //.setTangent(Math.toRadians(-120))
+                                .setReversed(true)
+                                .splineToLinearHeading(new Pose2d(-50, 20, Math.toRadians(-135)), Math.toRadians(-110))
+                                .waitSeconds(2)
+                                .setTangent(Math.toRadians(-135))
+                                .splineToLinearHeading(new Pose2d(-61, 12, Math.toRadians(180)), Math.toRadians(180))
+                                .waitSeconds(2)
+                                .setTangent(0)
+                                .splineToConstantHeading(new Vector2d(20, 12) , Math.toRadians(0))
+                                //.splineToLinearHeading(new Pose2d(46, 31, Math.toRadians(0)), Math.toRadians(45))
+                                .splineToConstantHeading(new Vector2d(42, 31), Math.toRadians(60))
+                                .waitSeconds(1)
+                                .setTangent(Math.toRadians(-135))
+                                .splineToConstantHeading(new Vector2d(20, 12), Math.toRadians(180))
+                                .splineToConstantHeading(new Vector2d(-61, 12), Math.toRadians(180))
+                                .waitSeconds(2)
+                                .setTangent(0)
+                                .splineToConstantHeading(new Vector2d(20, 12) , Math.toRadians(0))
+                                .splineToConstantHeading(new Vector2d(42, 31), Math.toRadians(60))
+                                .waitSeconds(1)
+                                .setTangent(Math.toRadians(-135))
+                                .splineToConstantHeading(new Vector2d(20, 12), Math.toRadians(180))
+                                .splineToConstantHeading(new Vector2d(-61, 12), Math.toRadians(180))
+                                .waitSeconds(2)
+                                .setTangent(0)
+                                .splineToConstantHeading(new Vector2d(20, 12) , Math.toRadians(0))
+                                .splineToConstantHeading(new Vector2d(42, 31), Math.toRadians(60))
+                                .waitSeconds(1)
+
+//                                .setReversed(true)
+//                                .setTangent(Math.toRadians(-110))
+//                                .splineToSplineHeading(new Pose2d(-42,24, Math.toRadians(180)), Math.toRadians(-30))
+//                                .waitSeconds(1)
+//                                .setTangent(Math.toRadians(180))
+                                //.splineToSplineHeading(new Pose2d(-32, 24, Math.toRadians(180)), Math.toRadians(-90))
+//                                .splineToConstantHeading(new Vector2d(-42, 12), Math.toRadians(0))
+//                                .waitSeconds(1)
+//                                .setTangent(0)
+//                                .splineToSplineHeading(new Pose2d(20, 12, Math.toRadians(180)), Math.toRadians(0))
+//                                .splineToSplineHeading(new Pose2d(48, 36, Math.toRadians(180)), Math.toRadians(60))
 //
+
+
+
+
+
+                                // Blue close
 //                                .setReversed(true)
-//                                .splineToSplineHeading(new Pose2d(43, 42, Math.toRadians(180)), Math.toRadians(0))
-//                                .waitSeconds(2.5) // Place Yellow
+////                                .UNSTABLE_addTemporalMarkerOffset(0, () -> {
+////                                    robot.intake.tiltUp();
+////                                    robot.outtake.openBothClaw();
+////                                })
+////                                .UNSTABLE_addTemporalMarkerOffset(0.75, () -> {robot.outtake.v4barTransfer();})
+////                                .UNSTABLE_addTemporalMarkerOffset(1.25, () -> {robot.outtake.closeBothClaw();})
+////                                .UNSTABLE_addTemporalMarkerOffset(1.5, ()->{
+////                                    robot.intake.tiltDown();
+////                                    robot.outtake.v4barScore();
+////                                })
+//                                .splineToSplineHeading(new Pose2d(23, 34, Math.toRadians(90)), Math.toRadians(-90))
+////                                .UNSTABLE_addTemporalMarkerOffset(0.5, () -> {robot.special.releasePixel();})
+//                                //.UNSTABLE_addTemporalMarkerOffset(0, () -> {robot.transfer();})
+//                                .waitSeconds(1)
+//
 //
 //                                .setReversed(false)
-//                                .lineToLinearHeading(new Pose2d(46, 58, Math.toRadians(-125))) // Corner Park
-////                                .lineToLinearHeading(new Pose2d(43, 13, Math.toRadians(-155))) // Middle Park
+////                                .UNSTABLE_addTemporalMarkerOffset(-0.5, () -> {robot.outtake.v4barScore();})
+//                                .splineToSplineHeading(new Pose2d(48, 42, Math.toRadians(180)), Math.toRadians(0))
+////                                .UNSTABLE_addTemporalMarkerOffset(0.5, () -> {robot.outtake.openBothClaw();})
+//                                .waitSeconds(1.5)
+//
+//
+////                                .UNSTABLE_addTemporalMarkerOffset(-0.5, ()->{robot.outtake.v4barStow();})
+//                                .waitSeconds(0.5)
+//                                .forward(3)
+//                                .lineToLinearHeading(new Pose2d(45, 12, Math.toRadians(-155)))
 
-                                .setReversed(true)
-                                .splineToSplineHeading(new Pose2d(-31, -31, Math.toRadians(180)), Math.toRadians(45))
-                                .waitSeconds(1) // Score Purple Spike
-
-
-                                .setReversed(false)
-                                .splineToConstantHeading(new Vector2d(-60, -11.5), Math.toRadians(180))
-//                                .splineToSplineHeading(new Pose2d(-60, -11.5, Math.toRadians(180)), Math.toRadians(180))
-                                .waitSeconds(2) // Score Yellow Spike
-
-
-                                // Drive to Board
-                                .setReversed(true)
-                                .splineToSplineHeading(new Pose2d(32, -11.5, Math.toRadians(180)), Math.toRadians(0))
-                                .waitSeconds(0.5)
-                                .lineToLinearHeading(new Pose2d(43, -42, Math.toRadians(180)))
-//                                .splineToConstantHeading(new Vector2d(43, -42), Math.toRadians(-70))
-//                                .splineToSplineHeading(new Pose2d(43, -42, Math.toRadians(180)), Math.toRadians(-90))
-
-//                                .lineToLinearHeading(new Pose2d(43, -13, Math.toRadians(155)))
-
-
-
+//                                .setReversed(true)
+//                                .setTangent(Math.toRadians(80))
+////                                .UNSTABLE_addTemporalMarkerOffset(0.75, ()->{robot.jankOuttake.armDown();})
+////                                .UNSTABLE_addTemporalMarkerOffset(0, ()->{robot.jankOuttake.closeBoth();})
+//                                .splineToSplineHeading(new Pose2d(-32,-36, Math.toRadians(-160)), Math.toRadians(30))
+////                                .UNSTABLE_addTemporalMarkerOffset(0, ()->{robot.jankOuttake.openLeft();})
+////                                .UNSTABLE_addTemporalMarkerOffset(1.5, ()->{robot.jankOuttake.scoreArm();})
+//                                .waitSeconds(1.5)
+//                                .setTangent(Math.toRadians(180))
+//                                //.splineToSplineHeading(new Pose2d(-32, 24, Math.toRadians(180)), Math.toRadians(-90))
+//                                .splineToConstantHeading(new Vector2d(-32, -12), Math.toRadians(0))
+//                                .waitSeconds(1)
+//                                .setTangent(0)
+//                                .splineToSplineHeading(new Pose2d(20, -12, Math.toRadians(180)), Math.toRadians(0))
+//                                .splineToSplineHeading(new Pose2d(48, -30, Math.toRadians(180)), Math.toRadians(-60))
+////                                .UNSTABLE_addTemporalMarkerOffset(1.5, ()->{robot.jankOuttake.openRight();})
+//                                .forward(4)
+//                                .strafeLeft(10)
 
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jankbot/prototype/intakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jankbot/prototype/intakeTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.jankbot.testing;
+package org.firstinspires.ftc.teamcode.jankbot.prototype;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
