ea96a0f10a54439c55cf9b695ce683121f55897a
Arav-R <63694299+Arav-R@users.noreply.github.com>
2024-02-18 21:40:12 -0600

    add run without encoders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LM2/OpModes/TeleOp/AravControls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LM2/OpModes/TeleOp/AravControls.java
@@ -76,6 +76,11 @@ public class AravControls extends LinearOpMode {
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRight");
         DcMotor backRightMotor = hardwareMap.dcMotor.get("backRight");
 
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
         frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
