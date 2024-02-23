0a43fb321b315e8f2b9d3f16ce973c87d06e38e2
Arav-R <63694299+Arav-R@users.noreply.github.com>
2024-02-23 08:38:32 -0600

    added telem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LM2/LinkageOpModes/TeleOp/LinkageTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LM2/LinkageOpModes/TeleOp/LinkageTele.java
@@ -355,6 +355,13 @@ public class LinkageTele extends LinearOpMode {
             leftTrigger.readValue();
             rightTrigger.readValue();
 
+            telemetry.addData("Left Y", -gamepad1.left_stick_y);
+            telemetry.addData("Left X", gamepad1.left_stick_x);
+            telemetry.addData("Right X", gamepad1.right_stick_x);
+
+            telemetry.addData("Drive", y);
+            telemetry.addData("Strafe", x);
+            telemetry.addData("Turn", rx);
 
             telemetry.addData("State", machine.getState());
             telemetry.addData("Arm Position", robot.arm.getCurrentPosition());
