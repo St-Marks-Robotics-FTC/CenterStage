5a6161e9c2f0ad3b859336c20ea1aed0f2c79954
Arav-R <63694299+Arav-R@users.noreply.github.com>
2024-03-05 17:40:23 -0600

    changed to use a state machine

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LebronBot/ChassisTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LebronBot/ChassisTest.java
@@ -3,19 +3,28 @@ package org.firstinspires.ftc.teamcode.LebronBot;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.arcrobotics.ftclib.gamepad.TriggerReader;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.sfdev.assembly.state.StateMachine;
+import com.sfdev.assembly.state.StateMachineBuilder;
 
 import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 
 @TeleOp(group = "Testing")
-public class ChassisTest extends OpMode {
+public class ChassisTest extends LinearOpMode {
 
+    public enum LinearStates {
+        IDLE,
+        DOWN,
+        UP,
+        OUTTAKE
+    }
 
     boolean up = false;
     String pivot = "ground";
@@ -38,7 +47,7 @@ public class ChassisTest extends OpMode {
     Servo intakeAngle;
 
     @Override
-    public void init() {
+    public void runOpMode() throws InterruptedException {
         frontLeftMotor = hardwareMap.dcMotor.get("frontLeft");
         backLeftMotor = hardwareMap.dcMotor.get("backLeft");
         frontRightMotor = hardwareMap.dcMotor.get("frontRight");
@@ -53,7 +62,7 @@ public class ChassisTest extends OpMode {
         backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
 
         intake = hardwareMap.get(DcMotorEx.class, "intake");
-        intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         intake.setDirection(DcMotorSimple.Direction.REVERSE);
 
         slide = hardwareMap.dcMotor.get("slide");
@@ -73,80 +82,107 @@ public class ChassisTest extends OpMode {
                 pad1, GamepadKeys.Trigger.RIGHT_TRIGGER
         );
 
-    }
+        // MAIN State Machine
+        StateMachine machine = new StateMachineBuilder()
+                .state(LinearStates.IDLE)                 // Driving to wing to pick up
+                .onEnter(() -> { // Happens on Init as well
+                    intake.setPower(0);
+                    intakeAngle.setPosition(0.45); // up
+                })
+                .transition(() -> gamepad1.right_trigger >= 0.05) // Drop intake down
+                .transition(() -> gamepad1.left_trigger >= 0.05, LinearStates.OUTTAKE) // Drop intake down
 
-    @Override
-    public void loop() {
-        double tranScaleFactor = gamepad1.left_bumper ? 0.4 : 1.0;
-        double rotScaleFactor = gamepad1.left_bumper ? 0.3 : 1.0;
-
-        double y = -gamepad1.left_stick_y * tranScaleFactor;
-        double x = gamepad1.left_stick_x * tranScaleFactor;
-        double rx = gamepad1.right_stick_x * rotScaleFactor;
-
-
-
-        // Denominator is the largest motor power (absolute value) or 1
-        // This ensures all the powers maintain the same ratio,
-        // but only if at least one is out of the range [-1, 1]
-        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-        double frontLeftPower = (y + x + rx) / denominator * tranScaleFactor;
-        double backLeftPower = (y - x + rx) / denominator * tranScaleFactor;
-        double frontRightPower = (y - x - rx) / denominator * tranScaleFactor;
-        double backRightPower = (y + x - rx) / denominator * tranScaleFactor;
-
-        frontLeftMotor.setPower(maxPow*frontLeftPower);
-        backLeftMotor.setPower(maxPow*backLeftPower);
-        frontRightMotor.setPower(maxPow*frontRightPower);
-        backRightMotor.setPower(maxPow* backRightPower);
-
-//        intake.setPower(gamepad1.right_trigger - gamepad1.left_trigger);
+                .state(LinearStates.DOWN)
+                .loop(() -> {
+                    intake.setPower(1 * (gamepad1.right_trigger));
+                    if (pivot.equals("ground")) {
+                        intakeAngle.setPosition(0.8);
+                    } else if (pivot.equals("stack")) {
+                        intakeAngle.setPosition(0.71);
+                    }
+                })
+                .transition(() -> gamepad1.right_trigger < 0.05) // Picks up when trigger let go
+                .state(LinearStates.UP)
+                .onEnter(() -> {
+                    intake.setPower(0.45);
+                    intakeAngle.setPosition(0.45); // up
+                })
+                .transitionTimed(1, LinearStates.IDLE)
 
 
-        if (pad1.wasJustPressed(GamepadKeys.Button.A)) {
-            pivot = "ground";
-        } else if (pad1.wasJustPressed(GamepadKeys.Button.B)) {
-            pivot = "stack";
-        }
-
-        if (gamepad1.right_trigger > 0.1) {
-            intake.setPower(gamepad1.right_trigger);
-            if (pivot.equals("ground")) {
-                intakeAngle.setPosition(0.8);
-            } else if (pivot.equals("stack")) {
-                intakeAngle.setPosition(0.73);
-            }
-        } else if (gamepad1.left_trigger > 0.1) {
-            intake.setPower(-gamepad1.left_trigger);
-        } else {
-            intake.setPower(0);
-            intakeAngle.setPosition(0.45); // up
-        }
-
-
-        if (pad1.wasJustPressed(GamepadKeys.Button.RIGHT_BUMPER)) {
-            up = !up;
-        }
-        if (up) {
-            slide.setTargetPosition(485);
-            slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            slide.setPower(1);
-        } else {
-            slide.setTargetPosition(5);
-            slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            slide.setPower(1);
-        }
+                .state(LinearStates.OUTTAKE)
+                .onEnter(() -> {
+                    intake.setPower(-0.5);
+                    intakeAngle.setPosition(0.8);
+                })
+                .transition(() -> gamepad1.left_trigger < 0.05, LinearStates.IDLE) // Picks up when trigger let go
+                .build();
 
 
 
-        pad1.readButtons();
-        leftTrigger.readValue();
-        rightTrigger.readValue();
 
 
-        telemetry.addData("slide pos", slide.getCurrentPosition());
-        telemetry.addData("Intake Power", intake.getPower());
-        telemetry.addData("Intake Current", intake.getCurrent(CurrentUnit.AMPS));
+
+        telemetry.addData("Status", "Initialized");
         telemetry.update();
+
+        waitForStart();
+
+        machine.start();
+
+        while (opModeIsActive()) {
+
+
+            double tranScaleFactor = gamepad1.left_bumper ? 0.4 : 1.0;
+            double rotScaleFactor = gamepad1.left_bumper ? 0.3 : 1.0;
+
+            double y = -gamepad1.left_stick_y * tranScaleFactor;
+            double x = gamepad1.left_stick_x * tranScaleFactor;
+            double rx = gamepad1.right_stick_x * rotScaleFactor;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator * tranScaleFactor;
+            double backLeftPower = (y - x + rx) / denominator * tranScaleFactor;
+            double frontRightPower = (y - x - rx) / denominator * tranScaleFactor;
+            double backRightPower = (y + x - rx) / denominator * tranScaleFactor;
+
+            frontLeftMotor.setPower(maxPow * frontLeftPower);
+            backLeftMotor.setPower(maxPow * backLeftPower);
+            frontRightMotor.setPower(maxPow * frontRightPower);
+            backRightMotor.setPower(maxPow * backRightPower);
+
+            if (pad1.wasJustPressed(GamepadKeys.Button.A)) {
+                pivot = "ground";
+            } else if (pad1.wasJustPressed(GamepadKeys.Button.B)) {
+                pivot = "stack";
+            }
+
+            machine.update();
+
+            if (pad1.wasJustPressed(GamepadKeys.Button.RIGHT_BUMPER)) {
+                up = !up;
+            }
+            if (up) {
+                slide.setTargetPosition(485);
+                slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                slide.setPower(1);
+            } else {
+                slide.setTargetPosition(5);
+                slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                slide.setPower(1);
+            }
+
+            pad1.readButtons();
+            leftTrigger.readValue();
+            rightTrigger.readValue();
+
+            telemetry.addData("slide pos", slide.getCurrentPosition());
+            telemetry.addData("Intake Power", intake.getPower());
+            telemetry.addData("Intake Current", intake.getCurrent(CurrentUnit.AMPS));
+            telemetry.update();
+        }
     }
 }
