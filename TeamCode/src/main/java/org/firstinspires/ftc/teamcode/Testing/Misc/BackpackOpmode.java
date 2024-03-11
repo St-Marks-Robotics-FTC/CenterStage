0eabc9d87c8887463cf1567c84629cb929550f0b
Arav-R <63694299+Arav-R@users.noreply.github.com>
2024-03-11 15:26:12 -0500

    Create BackpackOpmode.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/Misc/BackpackOpmode.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.Testing.Misc;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/*
+ * Created by Dryw Wade
+ *
+ * OpMode for testing Adafruit's MCP9808 temperature sensor driver
+ */
+@TeleOp(name = "MCP9808", group = "Tests")
+public class BackpackOpmode extends LinearOpMode
+{
+    private AdafruitLEDBackpack adafruitLEDBackpack;
+
+    public void runOpMode() throws InterruptedException
+    {
+        adafruitLEDBackpack = hardwareMap.get(AdafruitLEDBackpack.class, "backpack");
+
+        // Uncomment to use parameter version of driver class. This will require you to respecify
+        // the sensor type from MCP9808 to MCP9808Params
+//        MCP9808Params.Parameters parameters = new MCP9808Params.Parameters();
+//        parameters.hysteresis = MCP9808Params.Hysteresis.HYST_1_5;
+//        parameters.alertControl = MCP9808Params.AlertControl.ALERT_ENABLE;
+//        tempSensor.initialize(parameters);
+
+        adafruitLEDBackpack.allOn();
+        waitForStart();
+
+        while(opModeIsActive())
+        {
+//            telemetry.addData("Temperature", adafruitLEDBackpack.getTemperature());
+//            telemetry.addData("", "");
+//
+//            telemetry.addData("Lower Limit", adafruitLEDBackpack.getTemperatureLimit(MCP9808.Register.T_LIMIT_LOWER));
+//            telemetry.addData("Lower Limit Triggered", adafruitLEDBackpack.lowerLimitTriggered());
+//            telemetry.addData("Upper Limit", adafruitLEDBackpack.getTemperatureLimit(MCP9808.Register.T_LIMIT_UPPER));
+//            telemetry.addData("Upper Limit Triggered", adafruitLEDBackpack.upperLimitTriggered());
+//            telemetry.addData("Critical Limit", adafruitLEDBackpack.getTemperatureLimit(MCP9808.Register.T_LIMIT_CRITICAL));
+//            telemetry.addData("Critical Limit Triggered", adafruitLEDBackpack.criticalLimitTriggered());
+//            telemetry.addData("", "");
+//
+//            telemetry.addData("Config", Integer.toHexString(adafruitLEDBackpack.readShort(MCP9808.Register.CONFIGURATION)));
+//            telemetry.addData("Manufacturer ID", adafruitLEDBackpack.getManufacturerIDRaw());
+
+            telemetry.update();
+            idle();
+        }
+    }
+}
