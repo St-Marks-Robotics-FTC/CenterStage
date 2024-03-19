3e017c2a6167e950503b435cc31fb859aaf9f509
Arav-R <63694299+Arav-R@users.noreply.github.com>
2024-03-19 00:58:50 -0500

    got rid of Hang motor and preload servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LebronBot/Subsystems/SpecialTeams.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LebronBot/Subsystems/SpecialTeams.java
@@ -9,61 +9,19 @@ import com.qualcomm.robotcore.hardware.Servo;
 @Config
 public class SpecialTeams {
 
-    Servo preload;
-    DcMotorEx hang;
+
     Servo drone;
 
-
-    // Preload
-    public static double preloadGrab = 0.3;
-    public static double preloadRelease = 0.6;
-
-    // Hang
-    public static int hangPrepare = 700;
-    public static int hangClimb = 1200;
-    public static double hangPower = 1.0;
 
     // Drone
     public static double holdPos = 0.0;
     public static double releasePos = 0.5;
 
     public SpecialTeams (HardwareMap hardwareMap) {
-        hang = hardwareMap.get(DcMotorEx.class, "hang");
         drone = hardwareMap.get(Servo.class, "drone");
-        preload = hardwareMap.get(Servo.class, "preload");
 
-        hang.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        hang.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
-
-    //Preload pixel
-    public void holdPixel() {
-        preload.setPosition(preloadGrab);
-    }
-
-    public void releasePixel() {
-        preload.setPosition(preloadRelease);
-    }
-
-    // Hang
-    public void hangReady() {
-        setHang(hangPrepare);
-    }
-
-    public void hangClimb() {
-        setHang(hangClimb);
-    }
-
-    public int getHangPos() {
-        return hang.getCurrentPosition();
-    }
-
-    public void setHang(int pos) {
-        hang.setTargetPosition(pos);
-        hang.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        hang.setPower(hangPower);
-    }
-
+    
 
     // Drone
     public void holdDrone() {
