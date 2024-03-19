b6c3ed433126a1c040754e7b157c47da10eefda5
Arav-R <63694299+Arav-R@users.noreply.github.com>
2024-03-19 00:56:01 -0500

    added auto align

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LebronBot/Opmodes/TeleOp/LebronTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LebronBot/Opmodes/TeleOp/LebronTele.java
@@ -7,18 +7,23 @@ import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.control.PIDFController;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.arcrobotics.ftclib.controller.PIDController;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.arcrobotics.ftclib.gamepad.ToggleButtonReader;
 import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.sfdev.assembly.state.StateMachine;
 import com.sfdev.assembly.state.StateMachineBuilder;
 
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.LM2.Roadrunner.DriveConstants;
 import org.firstinspires.ftc.teamcode.LebronBot.LebronClass;
 import org.firstinspires.ftc.teamcode.LebronBot.Roadrunner.MecanumDrive;
 
@@ -55,8 +60,25 @@ public class LebronTele extends LinearOpMode {
     LebronClass robot;
     GamepadEx pad1, pad2;
 
-    private PIDFController headingController = new PIDFController(MecanumDrive.HEADING_PID);
-    double imuSetpoint = 180;
+
+    // Heading Auto Align
+    private PIDController controller;
+
+    // The IMU sensor object
+    private IMU imu;
+
+    public static double p = 0.7, i = 0, d = 0.05;
+    public static double f = 0;
+
+    public static double targetAngle = Math.toRadians(0);
+    double currAngle;
+
+    boolean PID = false;
+
+    double pidPower = 0;
+    double correctionPower = 0;
+
+    boolean stickZero = false;
 
 
 
@@ -77,26 +99,20 @@ public class LebronTele extends LinearOpMode {
         ToggleButtonReader hangToggle = new ToggleButtonReader(
                 pad1, GamepadKeys.Button.DPAD_RIGHT
         );
-//        ToggleButtonReader headingToggle = new ToggleButtonReader(
-//                pad1, GamepadKeys.Button.RIGHT_STICK_BUTTON
-//        );
-
-
         pad2 = new GamepadEx(gamepad2);
 
 
+        // PID
+        controller = new PIDController(p, i , d);
+
+        // TODO: adjust the names of the following hardware devices to match your configuration
+        imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                DriveConstants.LOGO_FACING_DIR, DriveConstants.USB_FACING_DIR));
+        imu.initialize(parameters);
+
+
         ElapsedTime time = new ElapsedTime();
-
-
-
-
-        // Retrieve our pose from the PoseStorage.currentPose static field
-        // See AutoTransferPose.java for further details
-//        robot.drive.getLocalizer().setPoseEstimate(PoseStorage.currentPose);
-
-        // Set input bounds for the heading controller
-        // Automatically handles overflow
-        headingController.setInputBounds(-Math.PI, Math.PI);
 
 
 
@@ -137,7 +153,6 @@ public class LebronTele extends LinearOpMode {
                     robot.intake.setIntake(0); // Stop Intake
                     robot.intake.tiltUp(); // Intake tilts up
                 })
-                .transition( () ->  robot.intake.is2NotAligned(), JamStates.MAIN) // JAM failsafe
                 .transitionTimed(0.75)
                 .transition( () ->  robot.intake.isTiltUp()) // Tilt is up
                 .transition( () ->  gamepad1.right_trigger > 0.5 , LinearStates.INTAKE) // Intake Again if we missed
@@ -238,6 +253,8 @@ public class LebronTele extends LinearOpMode {
 
         waitForStart();
 
+        targetAngle = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
         robot.special.holdDrone();
         machine.start();
 
@@ -251,37 +268,73 @@ public class LebronTele extends LinearOpMode {
             }
 
 
-            Pose2d poseEstimate = robot.drive.getPoseEstimate();
+            currAngle = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+//            currAngle = AngleUnit.normalizeRadians(currAngle);
 
-            double y = -gamepad1.left_stick_y;
-            double x = -gamepad1.left_stick_x * 1.1;
-            double rx = -gamepad1.right_stick_x * 0.8;
+            if (gamepad1.dpad_down) {
+                targetAngle = currAngle;
+            }
+
+
+
 
             double tranScaleFactor = gamepad1.left_bumper ? 0.4 : 1.0;
-            double rotScaleFactor = gamepad1.left_bumper ? 0.3 : 1.0;
-            if (gamepad1.left_trigger >= 0.5) {
-                headingController.setTargetPosition(Math.toRadians(imuSetpoint));
+            double rotScaleFactor = gamepad1.left_bumper ? 0.4 : 0.9;
 
-                // Set desired angular velocity to the heading controller output + angular
-                // velocity feedforward
-                double headingInput = clamp(headingController.update(poseEstimate.getHeading()), -0.3, 0.3);
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x;
+            // Apply scaling factors
+            y *= tranScaleFactor;
+            x *= tranScaleFactor;
 
-                robot.drive.setWeightedDrivePower(
-                        new Pose2d(tranScaleFactor * y, tranScaleFactor * x, headingInput)
-                );
+            if (gamepad1.right_stick_button || gamepad1.y) {
+                PID = true;
+                stickZero = false;
+            } else if (gamepad1.a) {
+                PID = false;
+            }
 
+            double rx;
+
+            if (PID) {
+                if (Math.abs(gamepad1.right_stick_x) < 0.01) {
+                    stickZero = true;
+                }
+                if (Math.abs(gamepad1.right_stick_x) > 0.1 && stickZero) {
+                    PID = false;
+                }
+
+
+                controller.setPID(p, i, d);
+                pidPower = controller.calculate(AngleUnit.normalizeRadians(currAngle - targetAngle), 0);
+
+                correctionPower = pidPower + f;
+//                if (controller.getPositionError() < Math.toRadians(5)) { // 5 degree tolerace
+//                    correctionPower = 0;
+//                }
+                rx = -correctionPower;
             } else {
-                robot.drive.setWeightedDrivePower(
-                        new Pose2d(tranScaleFactor * y, tranScaleFactor * x, rotScaleFactor * rx)
-                );
+                rx = gamepad1.right_stick_x;
+                rx *= rotScaleFactor;
             }
 
-            // Heading PID Adjustments
-            if (pad2.wasJustPressed(GamepadKeys.Button.DPAD_RIGHT) || pad1.wasJustPressed(GamepadKeys.Button.DPAD_RIGHT)) {
-                imuSetpoint += 2.5; // degrees
-            } else if (pad2.wasJustPressed(GamepadKeys.Button.DPAD_LEFT) || pad1.wasJustPressed(GamepadKeys.Button.DPAD_LEFT)) {
-                imuSetpoint -= 2.5; // degrees
-            }
+
+
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio, but only when
+            // at least one is out of the range [-1, 1]
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            robot.drive.setMotorPowers(frontLeftPower, backLeftPower, frontRightPower, backRightPower);
+
+
+
 
 
 
@@ -333,12 +386,6 @@ public class LebronTele extends LinearOpMode {
             telemetry.addData("Slide Level", slideLevel);
             telemetry.addData("Turret Pos", turretLevel);
             telemetry.addData("Manual Slides", manualSlides);
-            telemetry.addData("Heading Setpoint", imuSetpoint);
-
-
-            telemetry.addData("x", poseEstimate.getX());
-            telemetry.addData("y", poseEstimate.getY());
-            telemetry.addData("heading", poseEstimate.getHeading());
 
 
             // in da loop
