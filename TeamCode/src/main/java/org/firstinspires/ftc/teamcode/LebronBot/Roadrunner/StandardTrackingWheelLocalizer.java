4198d20db448dd173719b84161efaf5b1e5b7d3d
Arav-R <63694299+Arav-R@users.noreply.github.com>
2024-05-04 21:05:18 -0500

    more precise odo radius

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LebronBot/Roadrunner/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LebronBot/Roadrunner/StandardTrackingWheelLocalizer.java
@@ -29,7 +29,7 @@ import java.util.List;
 @Config
 public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer {
     public static double TICKS_PER_REV = 2000;
-    public static double WHEEL_RADIUS = 0.945; // in
+    public static double WHEEL_RADIUS = 0.944882; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
     public static double LATERAL_DISTANCE = 11.773; // in; distance between the left and right wheels
